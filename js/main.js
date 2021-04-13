c37da47c845ed0e5e2d752285649605bd1ad5239
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-04-12 21:19:55 -0500

    almost done
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- js/main.js
+++ js/main.js
@@ -17,6 +17,10 @@ const result = {
   error: 0,
   repetition: 0,
 };
+const settingCurrentLevel = (level)=>{
+  currentLevel = level;
+  currentLevel.startLevel();
+}
 //win and loose general functions
 const handleLoose = () => {
   mainScreen.classList.add('hidden');
@@ -27,6 +31,7 @@ const handleWin = () => {
   winScreen.classList.remove('hidden');
 }
 //creating the levels
+//level 1 ---------------------------------------------------------
 const level1 = createLevel({
   matrix,
   pattern: [0, 1, 2, 3],
@@ -34,20 +39,20 @@ const level1 = createLevel({
     first: [153, 179, 250],
     second: [11, 34, 98]
   },
-  size: 5,
+  size: 3,
   time: 20,
   timerDisplay: timerText,
   finishLevelFunc: () => {
-    console.log('esta es una pruebaaa');
+    console.log('level 1 done');
     //setting current level
-    currentLevel = level2;
-    currentLevel.startLevel();
+    settingCurrentLevel(level2); 
   },
   lostByTimeFunc: () => {
     console.log('lost by time');
     handleLoose(); 
   }
 });
+// level 2 ---------------------------------------------------------
 const level2 = createLevel({
   matrix,
   pattern: [3, 2, 1, 0],
@@ -55,12 +60,12 @@ const level2 = createLevel({
     first: [153, 179, 250],
     second: [11, 34, 98]
   },
-  size: 2,
-  time: 10,
+  size: 4,
+  time: 25,
   timerDisplay: timerText,
   finishLevelFunc: () => {
-    console.log('esta es una pruebaaa');
-    handleWin(); 
+    console.log('level 2 done');
+    settingCurrentLevel(level3); 
     //result.error = +level1.variables.error + level2.variables.error;
     //console.log(result.error + 'total errors');
   },
@@ -69,6 +74,30 @@ const level2 = createLevel({
     handleLoose(); 
   }
 });
+//level 3 ---------------------------------------------------------
+const level3 = createLevel({
+  matrix,
+  pattern: [4,5,9,1],
+  colors: {
+    first: [153, 179, 250],
+    second: [11, 34, 98]
+  },
+  size: 5,
+  time: 30,
+  timerDisplay: timerText,
+  finishLevelFunc: () => {
+    console.log('level 3 done');
+    handleWin(); 
+  },
+  lostByTimeFunc: () => {
+    console.log('lost by time');
+    handleLoose(); 
+  }
+});
+
+
+
+
 //init level >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 currentLevel = level1;
 
@@ -85,7 +114,6 @@ button.addEventListener('click', () => {
   freezeClick = true;
   currentLevel.simonSays();
   currentLevel.variables.repetition++;
-  console.log(currentLevel.variables.repetition);
 });
 firstScreenBtn.addEventListener('click', () => {
   firstScreen.classList.add('hidden');
